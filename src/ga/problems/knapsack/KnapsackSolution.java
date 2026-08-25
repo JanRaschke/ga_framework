f4d8991f1216cae9e2c1074ee01976b58e8e0375
JanRaschke <janphilipraschke@gmail.com>
2026-08-25 14:01:18 +0200

    Finalize task 2-3.

--- src/ga/problems/knapsack/KnapsackSolution.java
+++ src/ga/problems/knapsack/KnapsackSolution.java
@@ -1,8 +1,11 @@
 package ga.problems.knapsack;
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public class KnapsackSolution  {
+
     private ArrayList<KnapsackItem> contents;
     private int totalValue;
     private int totalWeight;
@@ -13,24 +16,45 @@ public class KnapsackSolution  {
         totalWeight = 0;
     }
 
-    // Content Getter
+    public KnapsackSolution(KnapsackSolution other) {
+        this.contents = other.contents;
+        this.totalValue = other.totalValue;
+        this.totalWeight = other.totalWeight;
+    }
+
+    // Content Getter & Setter
     public ArrayList<KnapsackItem> getContents() {
         return contents;
     }
+    public void setContents(ArrayList<KnapsackItem> contents) {
+        this.contents = contents;
+    }
 
-    // TotalValue Getter
+    // TotalValue Getter & Setter
     public int getTotalValue() {
         return totalValue;
     }
+    public void setTotalValue(int totalValue) {
+        this.totalValue = totalValue;
+    }
 
-    // TotalWeight Getter
+    // TotalWeight Getter & Setter
     public int getTotalWeight() {
         return totalWeight;
+    }
+    public void setTotalWeight(int totalWeight) {
+        this.totalWeight = totalWeight;
     }
 
     public void addItemToContents(KnapsackItem item) {
         this.contents.add(item);
         this.totalValue += item.getValue();
         this.totalWeight += item.getWeight();
+    }
+
+    public void removeItemFromContents(KnapsackItem item) {
+        this.contents.remove(item);
+        this.totalValue -= item.getValue();
+        this.totalWeight -= item.getWeight();
     }
 }
