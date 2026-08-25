dc286d65923c8657e587e7d26c44f891e175c617
Nils <nils.gellert@outlook.de>
2026-08-25 10:41:36 +0200

    1.1.2

--- src/ga/framework/GeneticAlgorithm.java
+++ src/ga/framework/GeneticAlgorithm.java
@@ -2,17 +2,15 @@ package ga.framework;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import ga.framework.model.NoSolutionException;
 import ga.framework.model.Problem;
 import ga.framework.operators.EvolutionaryOperator;
 import ga.framework.operators.FitnessEvaluator;
 import ga.framework.operators.SurvivalOperator;
+import ga.framework.model.Solution;
 
-/**
- * Grundgeruest fuer den Genetischen Algorithmus (Aufgabe 1.1)
- */
 public class GeneticAlgorithm {
-    // Hier erarbeiten wir gleich gemeinsam die Attribute, Konstruktoren und
-    // Methoden!'
 
     private Problem problem;
     private int populationSize;
@@ -29,6 +27,30 @@ public class GeneticAlgorithm {
         this.fitnessEvaluator = fitnessEvaluator;
         this.survivalOperator = survivalOperator;
         this.maxIterations = maxIterations;
+    }
+
+    public List<Solution> runOptimization() {
+        try {
+            List<Solution> population = new ArrayList<>();
+            for (int i = 0; i < populationSize; i++) {
+
+                population.add(problem.createNewSolution());
+            }
+
+            fitnessEvaluator.evaluate(population);
+
+            for (int iteration = 0; iteration < maxIterations; iteration++) {
+                int randomIndex = random.nextInt(evolutionaryOperators.size()) - 1;
+                EvolutionaryOperator operator = evolutionaryOperators.get(randomIndex);
+            }
+
+            return population;
+
+        } catch (NoSolutionException e) {
+            System.err.println("Fehler Startpopulation " + e.getMessage());
+            return null;
+        }
+
     }
 
 }
