e558a467169b26ca5415864ced2b48f80ccd51cb
JanRaschke <janphilipraschke@gmail.com>
2026-08-25 11:26:56 +0200

    Finalize task 2-1.

--- src/ga/problems/knapsack/KnapsackProblem.java
+++ src/ga/problems/knapsack/KnapsackProblem.java
@@ -1,11 +1,18 @@
 package ga.problems.knapsack;
 
+import ga.framework.model.*;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class KnapsackProblem {
 
     private int capacity;
+    private ArrayList<KnapsackItem> items;
 
-    public KnapsackProblem(int capacity) {
+    public KnapsackProblem(int capacity, ArrayList<KnapsackItem> items) {
         this.capacity = capacity;
+        this.items = items;
     }
 
     //Implement Getter & Setter-Method for capacity variable
@@ -15,5 +22,26 @@ public class KnapsackProblem {
 
     public void setCapacity(int capacity) {
         this.capacity = capacity;
+    }
+
+    public KnapsackSolution createNewSolution() throws Exception {
+        KnapsackSolution solution = new KnapsackSolution();
+        ArrayList<KnapsackItem> remainingItems = items;
+
+        while (!remainingItems.isEmpty()) {
+            int randomIndex = ThreadLocalRandom.current().nextInt(0, remainingItems.size());
+            KnapsackItem item = remainingItems.get(randomIndex);
+
+            if(solution.getTotalWeight() + item.getWeight() > this.capacity) {
+                remainingItems.remove(randomIndex);
+            } else {
+                solution.addItemToContents(item);
+                remainingItems.remove(randomIndex);
+            }
+        }
+        if(solution.getContents().isEmpty()){
+            throw new NoSolutionException("Alle Elemente sind zu schwer!");
+        }
+        return solution;
     }
 }
--- src/ga/problems/knapsack/KnapsackSolution.java
+++ src/ga/problems/knapsack/KnapsackSolution.java
@@ -1,5 +1,36 @@
 package ga.problems.knapsack;
 
-public class KnapsackSolution {
+import java.util.ArrayList;
 
+public class KnapsackSolution  {
+    private ArrayList<KnapsackItem> contents;
+    private int totalValue;
+    private int totalWeight;
+
+    public KnapsackSolution() {
+        contents = new ArrayList<>();
+        totalValue = 0;
+        totalWeight = 0;
+    }
+
+    // Content Getter
+    public ArrayList<KnapsackItem> getContents() {
+        return contents;
+    }
+
+    // TotalValue Getter
+    public int getTotalValue() {
+        return totalValue;
+    }
+
+    // TotalWeight Getter
+    public int getTotalWeight() {
+        return totalWeight;
+    }
+
+    public void addItemToContents(KnapsackItem item) {
+        this.contents.add(item);
+        this.totalValue += item.getValue();
+        this.totalWeight += item.getWeight();
+    }
 }
