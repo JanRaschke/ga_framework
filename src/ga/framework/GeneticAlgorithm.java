f4e8dd527e40950eef0cb70a582620763b665b76
Nils <nils.gellert@outlook.de>
2026-08-25 11:07:16 +0200

    1.2.1_final

--- src/ga/framework/GeneticAlgorithm.java
+++ src/ga/framework/GeneticAlgorithm.java
@@ -2,13 +2,15 @@ package ga.framework;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import ga.framework.model.NoSolutionException;
 import ga.framework.model.Problem;
+import ga.framework.model.Solution;
+import ga.framework.operators.EvolutionException;
 import ga.framework.operators.EvolutionaryOperator;
 import ga.framework.operators.FitnessEvaluator;
 import ga.framework.operators.SurvivalOperator;
-import ga.framework.model.Solution;
 
 public class GeneticAlgorithm {
 
@@ -30,27 +32,33 @@ public class GeneticAlgorithm {
     }
 
     public List<Solution> runOptimization() {
+        Random random = new Random();
         try {
             List<Solution> population = new ArrayList<>();
             for (int i = 0; i < populationSize; i++) {
-
                 population.add(problem.createNewSolution());
             }
 
             fitnessEvaluator.evaluate(population);
 
             for (int iteration = 0; iteration < maxIterations; iteration++) {
-                int randomIndex = random.nextInt(evolutionaryOperators.size()) - 1;
+                int randomIndex = random.nextInt(evolutionaryOperators.size());
                 EvolutionaryOperator operator = evolutionaryOperators.get(randomIndex);
+
+                List<Solution> children = new ArrayList<>();
+                for (Solution solution : population) {
+                    children.add(operator.evolve(solution));
+                }
             }
 
             return population;
 
+        } catch (EvolutionException e) {
+            System.err.println("Fehler bei Evolution: " + e.getMessage());
+            return null;
         } catch (NoSolutionException e) {
             System.err.println("Fehler Startpopulation " + e.getMessage());
             return null;
         }
-
     }
-
 }
